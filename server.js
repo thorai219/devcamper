d60bb8ba2df337122f1f0bfa2702abd266106408
Terry Lee <slsk0219@gmail.com>
2022-06-23 23:04:50 -0700

    set up colors for terminal add dummy data

--- server.js
+++ server.js
@@ -1,20 +1,27 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const morgan = require("morgan");
-const bootcampRouter = require("./routes/bootcamps");
+const express = require('express');
+const dotenv = require('dotenv');
+const colors = require('colors');
+const morgan = require('morgan');
+const connectDB = require('./config/db');
 
-dotenv.config({ path: "./config/config.env" });
+dotenv.config({ path: './config/config.env' });
 
+const bootcampRouter = require('./routes/bootcamps');
+
+connectDB();
 const app = express();
 
-if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
 }
-app.use("/api/v1/bootcamps", bootcampRouter);
+app.use('/api/v1/bootcamps', bootcampRouter);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
-  PORT,
-  console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
-);
+const server = app.listen(PORT, console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`.green.inverse.bold));
+
+// unhandled rejection
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  server.close(() => process.exit(1));
+});
